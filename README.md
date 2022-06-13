8822393f90e44a69c26f2dfdf28c523b975057a8
aloysiusyoko <aloysiusyoko@gmail.com>
2022-06-13 13:08:17 +0700

    add example and README

--- README.md
+++ README.md
@@ -2,3 +2,38 @@
 
 query builder for MySQL
 
+## Features
+
+* generate where clause and the arguments for `SELECT` query based on the params
+
+## Examples
+
+```Go
+
+type FooParam struct {
+    Name sql.NullString `param:"name" db:"name"`
+    Age  sql.NullInt64  `param:"age" db:"age"`
+}
+
+func GetFoo() {
+
+    // ...
+
+    p := FooParam{
+        Name: sql.NullString{Valid:true, String: "foo"}
+        Age: sql.NullInt64{Valid:true, Int64: 17}
+    }
+    
+    qb := qbuilder.New()
+    wc, args, err := qb.Build(&param)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("wc", wc)   // WHERE 1=1 AND name=? AND age=? LIMIT 0,10
+    fmt.Println("args", wc) // ["foo", 17]
+    
+    // ...
+}
+
+```
---
+++ example/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"strings"
+
+	"github.com/Tuingking/qbuilder"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	username = "root"
+	password = "password"
+	host     = "127.0.0.1"
+	port     = "3306"
+	database = "playground"
+)
+
+func main() {
+	connstr := "{username}:{password}@tcp({host}:{port})/{database}"
+
+	replacer := strings.NewReplacer(
+		"{username}", username,
+		"{password}", password,
+		"{host}", host,
+		"{port}", port,
+		"{database}", database,
+	)
+
+	dataSourceName := replacer.Replace(connstr)
+
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	query := `SELECT id, name FROM product`
+
+	param := ProductParam{
+		ID: sql.NullInt64{Int64: 1, Valid: true},
+	}
+
+	qb := qbuilder.New()
+	wc, args, err := qb.Build(&param)
+	if err != nil {
+		fmt.Println("failed query builder", err)
+		return
+	}
+
+	rows, err := db.QueryContext(context.Background(), query+wc, args...)
+	if err != nil {
+		fmt.Println("failed query", err)
+		return
+	}
+
+	var products []Product
+	for rows.Next() {
+		var p Product
+		if err = rows.Scan(&p.ID, &p.Name); err != nil {
+			fmt.Println("failed scan", err)
+		}
+		products = append(products, p)
+	}
+
+	fmt.Printf("🐶🐶[DEBUG]🐶🐶 products: %+v\n", products)
+}
+
+type Product struct {
+	ID   int64
+	Name string
+}
+
+type ProductParam struct {
+	ID   sql.NullInt64  `param:"id" db:"id"`
+	Name sql.NullString `param:"name" db:"name"`
+}
